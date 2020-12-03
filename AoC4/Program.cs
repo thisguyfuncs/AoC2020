90379758ff1e9ec6679ca560ef061f242f4b9d10
TGF <thisguystreams@gmail.com>
2020-12-03 13:43:41 -0600

    pushing up day 4 code

---
+++ AoC4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace AoC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = File.ReadAllText("input.txt").Split("\r\n").ToList();
+        }
+    }
+}
