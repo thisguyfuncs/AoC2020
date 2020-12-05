0069bb6410827b75f9d82c222d21d3bb4ed7c45e
TGF <thisguystreams@gmail.com>
2020-12-05 00:05:00 -0600

    no message

--- AoC5/Program.cs
+++ AoC5/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,38 +8,31 @@ namespace AoC5
     {
         static void Main(string[] args)
         {
-            var boardingPasses = File.ReadAllText("input.txt").Split("\r\n").ToList();
-
-            var seatIds = new List<decimal>();
-
-            foreach(var pass in boardingPasses)
-            {
-                decimal passRowMin = 0;
-                decimal passRowMax = 127;
-
-                decimal passColMin =  0;
-                decimal passColMax = 7;
-
-                foreach(var c in pass)
-                {
-                    if (c == 'F') passRowMax = Math.Floor( ((passRowMax - passRowMin) / 2) + passRowMin);
-                    if (c == 'B') passRowMin = Math.Ceiling( ((passRowMax - passRowMin) / 2) + passRowMin );
-
-                    if (c == 'L') passColMax = Math.Floor(((passColMax - passColMin) / 2) + passColMin);
-                    if (c == 'R') passColMin = Math.Ceiling(((passColMax - passColMin) / 2) + passColMin);
-                }
-
-                seatIds.Add( (passRowMax * 8) + passColMax);
-            }
-
+            var seatIds = File.ReadAllText("input.txt").Split("\r\n").ToList().Select(GetSeatId).OrderBy(x => x).Select(x => Convert.ToInt32(x)).ToList();
             Console.WriteLine($"High seat id: {seatIds.Max()}");
 
-            var ordered = seatIds.OrderBy(x => x).Select(x => Convert.ToInt32(x)).ToList();
-            var missing = Enumerable.Range(ordered.Min(), ordered.Max()).Except(ordered);
-
+            var missing = Enumerable.Range(seatIds.Min(), seatIds.Max()).Except(seatIds);
             var yourSeat = missing.Where(x => !missing.Contains(x - 1) && !missing.Contains(x + 1)).FirstOrDefault();
-
             Console.WriteLine($"your seat: {yourSeat}");
+        }
+
+        private static decimal GetSeatId(string pass)
+        {
+            decimal passRowMin, passRowMax, passColMin, passColMax;
+            passRowMin = passColMin = 0;
+            passRowMax = 127;
+            passColMax = 7;
+
+            foreach (var c in pass)
+            {
+                if (c == 'F') passRowMax = Math.Floor(((passRowMax - passRowMin) / 2) + passRowMin);
+                if (c == 'B') passRowMin = Math.Ceiling(((passRowMax - passRowMin) / 2) + passRowMin);
+
+                if (c == 'L') passColMax = Math.Floor(((passColMax - passColMin) / 2) + passColMin);
+                if (c == 'R') passColMin = Math.Ceiling(((passColMax - passColMin) / 2) + passColMin);
+            }
+
+            return (passRowMax * 8) + passColMax;
         }
     }
 }
