3f7f75149544ee3799a7c5eab8d818f43e6f3656
TGF <thisguystreams@gmail.com>
2020-12-04 23:53:23 -0600

    5

--- AoC5/Program.cs
+++ AoC5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -8,7 +9,41 @@ namespace AoC5
     {
         static void Main(string[] args)
         {
-            var rows = File.ReadAllText("input.txt").Split("\r\n\r\n").Select(x => x.Replace("\r\n", " ")).ToList();
+            var boardingPasses = File.ReadAllText("input.txt").Split("\r\n").ToList();
+
+            var seatIds = new List<decimal>();
+
+            foreach(var pass in boardingPasses)
+            {
+                decimal passRowMin = 0;
+                decimal passRowMax = 127;
+
+                decimal passColMin =  0;
+                decimal passColMax = 7;
+
+                foreach(var c in pass)
+                {
+                    if (c == 'F') passRowMax = Math.Floor( ((passRowMax - passRowMin) / 2) + passRowMin);
+                    if (c == 'B') passRowMin = Math.Ceiling( ((passRowMax - passRowMin) / 2) + passRowMin );
+
+                    if (c == 'L') passColMax = Math.Floor(((passColMax - passColMin) / 2) + passColMin);
+                    if (c == 'R') passColMin = Math.Ceiling(((passColMax - passColMin) / 2) + passColMin);
+                }
+
+                if (passRowMax == passRowMin && passColMax == passColMin)
+                {
+                    seatIds.Add( (passRowMax * 8) + passColMax);
+                }
+            }
+
+            Console.WriteLine($"High seat id: {seatIds.Max()}");
+
+            var ordered = seatIds.OrderBy(x => x).Select(x => Convert.ToInt32(x)).ToList();
+            var missing = Enumerable.Range(ordered.Min(), ordered.Max()).Except(ordered);
+
+            var yourSeat = missing.Where(x => !missing.Contains(x - 1) && !missing.Contains(x + 1)).FirstOrDefault();
+
+            Console.WriteLine($"your seat: {yourSeat}");
         }
     }
 }
