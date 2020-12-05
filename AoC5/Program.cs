943bcf809d072cb1d4e068689c41427a4e7af6c0
TGF <thisguystreams@gmail.com>
2020-12-05 00:55:22 -0600

    maybe better

--- AoC5/Program.cs
+++ AoC5/Program.cs
@@ -6,14 +6,16 @@ namespace AoC5
 {
     class Program
     {
+        static decimal Calc(decimal min, decimal max) => Math.Floor(((max - min) / 2) + min);
+
         static void Main(string[] args)
         {
             var seatIds = File.ReadAllText("input.txt").Split("\r\n").ToList().Select(GetSeatId).ToList();
-            Console.WriteLine($"High seat id: {seatIds.Max()}");
 
             var missing = Enumerable.Range(seatIds.Min(), seatIds.Max()).Except(seatIds);
             var yourSeat = missing.FirstOrDefault(x => !missing.Contains(x - 1) && !missing.Contains(x + 1));
-            Console.WriteLine($"your seat: {yourSeat}");
+
+            Console.WriteLine($"high: {seatIds.Max()}, your seat: {yourSeat}");
         }
 
         private static int GetSeatId(string pass)
@@ -25,11 +27,10 @@ namespace AoC5
 
             foreach (var c in pass)
             {
-                if (c == 'F') passRowMax = Math.Floor(((passRowMax - passRowMin) / 2) + passRowMin);
-                if (c == 'B') passRowMin = Math.Ceiling(((passRowMax - passRowMin) / 2) + passRowMin);
-
-                if (c == 'L') passColMax = Math.Floor(((passColMax - passColMin) / 2) + passColMin);
-                if (c == 'R') passColMin = Math.Ceiling(((passColMax - passColMin) / 2) + passColMin);
+                if (c == 'F') passRowMax = Calc(passRowMin, passRowMax);
+                if (c == 'B') passRowMin = Calc(passRowMin, passRowMax);
+                if (c == 'L') passColMax = Calc(passColMin, passColMax);
+                if (c == 'R') passColMin = Calc(passColMin, passColMax);
             }
 
             return Convert.ToInt32((passRowMax * 8) + passColMax);
